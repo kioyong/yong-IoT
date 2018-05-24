9064250b66ecfc4a794514eda1ebc7262bcc34d3
kioyong <yong.a.liang@accenture.com>
2018-05-24 21:41:19 +0800

    add demo:

--- src/test/java/com/yong/iot/FindUnsortedSubarrayTest.java
+++ src/test/java/com/yong/iot/FindUnsortedSubarrayTest.java
@@ -3,8 +3,7 @@ package com.yong.iot;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.*;
 
 @Slf4j
 public class FindUnsortedSubarrayTest {
@@ -108,4 +107,122 @@ public class FindUnsortedSubarrayTest {
         }
         return x == 0 && y == 0;
     }
+
+    @Test
+//    https://leetcode-cn.com/problems/self-dividing-numbers/description/
+    public void test7() {
+        int left = 1;
+        int right = 22;
+        List<Integer> integers = selfDividingNumbers(left, right);
+        log.debug("result = {}", integers);
+    }
+
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            boolean isDividingNumber = true;
+            String I = String.valueOf(i);
+            for (int j = 0; j < I.length(); j++) {
+                if (I.substring(j, j + 1).equals("0") || i % Integer.valueOf(I.substring(j, j + 1)) != 0) {
+                    isDividingNumber = false;
+                    break;
+                }
+            }
+            if (isDividingNumber) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    @Test
+    //https://leetcode-cn.com/problems/keyboard-row/description/
+    public void test8() {
+        String[] words = new String[]{"Hello", "Alaska", "Dad", "Peace"};
+        String[] result = findWords(words);
+        log.debug("result = {}", result);
+    }
+
+    public String[] findWords(String[] words) {
+        String line1 = "qwertyuiop";
+        String line2 = "asdfghjkl";
+        String line3 = "zxcvbnm";
+        List<String> list = new ArrayList<>();
+        for (String word : words) {
+            if (includeChar(word, line1)) list.add(word);
+            if (includeChar(word, line2)) list.add(word);
+            if (includeChar(word, line3)) list.add(word);
+        }
+        return list.toArray(new String[0]);
+    }
+
+    public boolean includeChar(String world, String line) {
+        for (int i = 0; i < world.length(); i++) {
+            if (!line.contains(world.substring(i, i + 1).toLowerCase())) return false;
+        }
+        return true;
+    }
+
+    @Test
+    //https://leetcode-cn.com/problems/max-points-on-a-line/description/
+    public void test9() {
+        Point[] points = new Point[]{new Point(1, 1), new Point(1, 1), new Point(2, 2), new Point(2, 2)};
+        int i = maxPoints(points);
+        log.debug("result = {}", i);
+    }
+
+    private int maxPoints(Point[] points) {
+        if (points.length == 1) return 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < points.length; i++) {
+            Map<String, Integer> newMap = new HashMap<>();
+            for (int j = i + 1; j < points.length; j++) {
+                float a = (points[j].x - points[i].x) == 0 ? 0 : ((points[j].y - points[i].y)) == 0 ? 0 : ((float) (points[j].y - points[i].y) / ((float) (points[j].x - points[i].x)));
+                float b = points[j].y - a * points[j].x;
+                String key1 = a == 0 ? String.valueOf(i) : String.valueOf(a).concat("_").concat(String.valueOf(b)).concat(String.valueOf(i));
+                String key2 = a == 0 ? String.valueOf(j) : String.valueOf(a).concat("_").concat(String.valueOf(b).concat(String.valueOf(j)));
+                if (!map.containsKey(key1)) {
+                    if (newMap.containsKey(key1)) {
+                        newMap.put(key1, newMap.get(key1) + 1);
+                    } else {
+                        newMap.put(key1, 2);
+                    }
+                }
+                if (!map.containsKey(key2)) {
+                    if (newMap.containsKey(key2)) {
+                        newMap.put(key2, newMap.get(key2) + 1);
+                    } else {
+                        newMap.put(key2, 2);
+                    }
+                }
+            }
+            map.putAll(newMap);
+        }
+        Set<String> strings = map.keySet();
+        int maxSize = 0;
+        for (String key : strings) {
+            if (map.get(key) > maxSize) {
+                maxSize = map.get(key);
+            }
+        }
+        return maxSize;
+    }
+
+    class Point {
+        int x;
+        int y;
+
+        Point() {
+            x = 0;
+            y = 0;
+        }
+
+        Point(int a, int b) {
+            x = a;
+            y = b;
+        }
+    }
+
+
 }
